189e6d8c7ac891645a75c074d7dac1c02234b66d
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-01-10 20:52:33 +0200

    Update BinarySearchTree.js

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -137,16 +137,15 @@ class BinarySearchTree {
 
 const bst = new BinarySearchTree();
 bst.add(5);
+bst.add(1);
 bst.add(2);
-bst.add(0);
 bst.add(3);
+bst.add(4);
 bst.add(6);
-bst.add(9);
 bst.add(7);
-console.log(bst.findMin());
-console.log(bst.findMax());
-console.log(bst.isPresent(2));
-console.log(bst.isPresent(4));
+bst.add(8);
+bst.add(9);
+bst.add(10);
 bst.remove(9);
 console.log(bst.isBinarySearchTree(bst));
 console.log(bst);
